e40a93e8d5e8888770d9cffdff74069cf71281a0
Ahmad wannous <ahmadwanous642@gmail.com>
2025-02-13 12:07:12 +0200

    Solving Exercises 2

--- tickets.py
+++ tickets.py
@@ -1,3 +1,14 @@
-#1) Exercise 1: Movie Ticket Price Calculator
+#Exercise 1: Movie Ticket Price Calculator
 
+age = int(input("Enter your age: "))
 
+if age <= 12:
+    price = 5
+elif age <=17:
+    price = 8
+elif age <=50:
+    price = 12
+else:
+    price = 7
+
+print(f"The ticket price is ${price}.")
